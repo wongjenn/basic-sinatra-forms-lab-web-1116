22fa514c3ed76c029c64ed7475f2956fc1fce42f
wongjenn <wongjenn@yahoo.com>
2016-12-15 16:19:18 -0500

    Done

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
+
+  get "/newteam" do
+    erb :newteam
+  end
+
+  post "/team" do
+    @team_members = params
+    erb :team
+  end
 
 
 end
